f16b2bd3422eb944ba99dc5ff251d41c5b38a7ed
Siripragada Dheeraj <dheerajsiripragada@gmail.com>
2025-03-08 12:05:04 +0530

    Added HR Operations Manager project

---
+++ hr_operations_manager/src/login.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Login() {
+    return (
+        <div>
+            <label htmlFor="email">Email</label>
+            <input type="text" id="email" />
+            <h1>Hello</h1>
+        </div>
+    );
+}
+
+export default Login;
